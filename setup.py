6248934b3eee96bafcbe06d0d43bd67a2fd57414
MnrSanele <34538246+MnrSanele@users.noreply.github.com>
2022-09-02 16:15:37 +0200

    second commit

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
    description='EDSA example python package',
    long_description=open('README.md').read(),
    install_requires=['numpy'],
-   url='https://github.com/<username>/<package-name>',
-   author='<Your Name',
-   author_email='<Your Name>'
+   url='https://github.com/MnrSanele/mypackage.git',
+   author='MnrSanele',
+   author_email='sanele.gumede@yahoo.com'
 )
